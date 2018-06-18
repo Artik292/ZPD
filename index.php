e4aea9fe075f2c9d630f3d0127add469b9b125d0
Artur Konevnikov <artik292@gmail.com>
2018-06-18 10:30:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Днюха Яны');
 $app->initLayout('Centered');
 $app->layout->template->del('Header');
-$app->layout->add(['Image',"https://img-fotki.yandex.ru/get/4914/110106211.d/0_142c6a_8ad8f2e7_orig",'small centered'],'Header');
+$app->layout->add(['Image',"https://img-fotki.yandex.ru/get/4914/110106211.d/0_142c6a_8ad8f2e7_orig",'big centered'],'Header');
 $form = $app->add(["Form"]);
-$form->addField(["ans","caption"=>"Ответ: "]);
+$form->addField("ans",["caption"=>"Ответ: "]);
 $form->onSubmit(function($form) {
   If ($form->model["ans"] == "лодка") {
     return $form->success('Самое популярное число в моих снапах');
